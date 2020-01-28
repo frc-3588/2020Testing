1cd94f40d951df70c5b39cb620e52c8b903168b6
Robotics User <lindberghrobotics@gmail.com>
2020-01-28 15:07:32 -0800

    Tai-In shooter, changed power of joystick to 0.4

--- TestShooterCode/src/main/java/org/usfirst/frc3588/TestShooterCode/commands/activateShooter.java
+++ TestShooterCode/src/main/java/org/usfirst/frc3588/TestShooterCode/commands/activateShooter.java
@@ -43,7 +43,7 @@ public class activateShooter extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override  
     protected void execute() {
-     Robot.shooter.setShooterTalon(Robot.oi.joystick.getRawAxis(1) * 0.5);
+     Robot.shooter.setShooterTalon(Robot.oi.joystick.getRawAxis(1) * 0.4);
     }
  
     // Make this return true when this Command no longer needs to run execute()
